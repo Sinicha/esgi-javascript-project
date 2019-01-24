4e9ab4b38a30cf87684a4555188904a30fadf68a
TharshanSivapalan <sivapalan.tharshan@gmail.com>
2019-01-25 00:57:30 +0100

    [FIX] Reservation add images

--- app/view/views/reservation.js
+++ app/view/views/reservation.js
@@ -2,22 +2,22 @@ export function reservation() {
     return `
     <section>
             <h1 class="h1-title">Reservation</h1>
-                <h2>Date + heure</h2>"
+                <h2>Mardi Session de 22h</h2>
                 <div class="box">
                     <div class="inthebox">
-                        <img src="img.jpg" alt="Prison Break" style="width:100%">
+                        <img src="images/prison-break.jpg" alt="Prison Break" style="width:100%">
                         <div class="container">
                         <p>Prison Break</p>
                         </div>
                     </div>
                     <div class="inthebox">
-                        <img src="img.jpg" alt="Lost" style="width:100%">
+                        <img src="images/Lost.jpg" alt="Lost" style="width:100%">
                         <div class="container">
                         <p>Lost</p>
                         </div>
                     </div>
                     <div class="inthebox">
-                        <img src="img.jpg" alt="Manoir Hanté" style="width:100%">
+                        <img src="images/manoir_hante.jpg" alt="Manoir Hanté" style="width:100%">
                         <div class="container">
                         <p>Manoir Hanté</p>
                         </div>
