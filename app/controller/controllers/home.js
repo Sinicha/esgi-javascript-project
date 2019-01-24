25708d897901adde1c0f0123ed1af3a308d74041
Sinicha <sinirados@gmail.com>
2019-01-24 02:08:56 +0100

    Add filter on manager

--- app/controller/controllers/home.js
+++ app/controller/controllers/home.js
@@ -1,11 +1,8 @@
 import {home} from "../../view/views/home.js"
-import User from "../../model/models/user.js";
 
 export class HomeController {
 
     static callView() {
-        let user = new User('John Doe');
-        user.save();
         return home();
     }
 
--- app/model/manager.js
+++ app/model/manager.js
@@ -1,8 +1,17 @@
+/**
+ * This class is the manager for database interaction
+ */
 export default class Manager {
     constructor() {
 
     }
 
+    /**
+     * Get the last ID of the table
+     *
+     * @param table
+     * @returns {number}
+     */
     lastId(table) {
         let lastId = localStorage.getItem("db." + table.toLowerCase() + ".lastid");
         if (lastId == null) {
@@ -12,6 +21,12 @@ export default class Manager {
         return parseInt(lastId);
     }
 
+    /**
+     * Increment ID on a table
+     *
+     * @param table
+     * @returns {number}
+     */
     incrementId(table) {
         let lastId = localStorage.getItem("db." + table.toLowerCase() + ".lastid");
         if (lastId == null || parseInt(lastId) < 1) {
@@ -25,6 +40,13 @@ export default class Manager {
 
     }
 
+    /**
+     * Find one object with Id
+     *
+     * @param table
+     * @param id
+     * @returns a object of the model or null
+     */
     findById(table, id) {
         let entries = localStorage.getItem("db." + table.toLowerCase() + ".entries");
         if (entries != null) {
@@ -42,17 +64,119 @@ export default class Manager {
         return null;
     }
 
-    getAll(table) {
+    /**
+     * Filter with OR on a Model Entry "Table"
+     *
+     * @param table
+     * @param filters, an object with 'Key_to_search' and 'op'eration and 'value'
+     * @returns array of results or null
+     */
+    filter(table, filters) {
+        if (typeof filters != "object") {
+            return null;
+        }
         let entries = localStorage.getItem("db." + table.toLowerCase() + ".entries");
-        if (entries != null && Array.isArray(entries)) {
-            return JSON.parse(entries);
+        if (entries != null) {
+            let pEntries = JSON.parse(entries);
+            let results = [];
+            if (Array.isArray(pEntries)) {
+                // Search and add resultat find to array
+                for (let k in filters) {
+                    for (let i = 0; i < pEntries.length; i++) {
+
+                        switch (filters[k]['op']) {
+                            case '<': {
+                                if (pEntries[i][k] < filters[k]['value']) {
+                                    results.push(pEntries[i]);
+                                }
+                                break;
+                            }
+                            case '<=': {
+                                if (pEntries[i][k] <= filters[k]['value']) {
+                                    results.push(pEntries[i]);
+                                }
+                                break;
+                            }
+                            case '==': {
+                                if (pEntries[i][k] == filters[k]['value']) {
+                                    results.push(pEntries[i]);
+                                }
+                                break;
+                            }
+                            case '>=': {
+                                if (pEntries[i][k] >= filters[k]['value']) {
+                                    results.push(pEntries[i]);
+                                }
+                                break;
+                            }
+                            case '>': {
+                                if (pEntries[i][k] > filters[k]['value']) {
+                                    results.push(pEntries[i]);
+                                }
+                                break;
+                            }
+                            case '!=': {
+                                if (pEntries[i][k] != filters[k]['value']) {
+                                    results.push(pEntries[i]);
+                                }
+                                break;
+                            }
+                            case 'in': {
+                                if (pEntries[i][k] in filters[k]['value']) {
+                                    results.push(pEntries[i]);
+                                }
+                                break;
+                            }
+                            default: {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Delete duplicate object found
+            results = results.filter((elem, pos, arr) => {
+                return arr.indexOf(elem) == pos;
+            });
+
+            return results;
         } else {
             localStorage.setItem("db." + table.toLowerCase() + ".entries", JSON.stringify([]));
-            return [];
         }
+        return null;
     }
 
+    /**
+     * Get All the Entries of a table
+     *
+     * @param table
+     * @returns {*}
+     */
+    getAll(table) {
+        let entries = localStorage.getItem("db." + table.toLowerCase() + ".entries");
+        if (entries != null) {
+            let pEntries = JSON.parse(entries);
+            if (Array.isArray(pEntries)) {
+                return pEntries;
+            }
+        }
+        localStorage.setItem("db." + table.toLowerCase() + ".entries", JSON.stringify([]));
+        return [];
+    }
+
+    /**
+     * Save one object to a table
+     *
+     * @param table
+     * @param object
+     * @returns success of save
+     */
     save(table, object) {
+        if (typeof object != 'object') {
+            return false;
+        }
+
         // Save
         object['id'] = this.lastId(table) + 1;
 
@@ -69,6 +193,7 @@ export default class Manager {
 
         // Increment Id
         this.incrementId(table);
-    }
 
+        return true;
+    }
 }
