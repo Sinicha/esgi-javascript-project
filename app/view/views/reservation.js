bb278004184dc8c1d6034ba76c47c363275350cc
TharshanSivapalan <sivapalan.tharshan@gmail.com>
2019-01-24 23:48:13 +0100

    Data Missing for test and he need more investigation

--- app/view/views/reservation.js
+++ app/view/views/reservation.js
@@ -1,3 +1,27 @@
 export function reservation() {
-    return "<section>Reservation Page Bienvenue</section>";
+    return `
+    <section>
+            <h1 class="h1-title">Reservation</h1>
+                <h2>Date + heure</h2>"
+                <div class="box">
+                    <div class="inthebox">
+                        <img src="img.jpg" alt="Prison Break" style="width:100%">
+                        <div class="container">
+                        <p>Prison Break</p>
+                        </div>
+                    </div>
+                    <div class="inthebox">
+                        <img src="img.jpg" alt="Lost" style="width:100%">
+                        <div class="container">
+                        <p>Lost</p>
+                        </div>
+                    </div>
+                    <div class="inthebox">
+                        <img src="img.jpg" alt="Manoir Hanté" style="width:100%">
+                        <div class="container">
+                        <p>Manoir Hanté</p>
+                        </div>
+                    </div>
+                </div>
+    </section>`;
 }
